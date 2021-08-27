1e397ec3aa95b91fb6cde1023611834f5b5d3cdf
fn79 <color0470@gmail.com>
2021-08-27 11:34:11 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,44 @@
-// TODO
+use chrono::prelude::*;
+use colored::*;
+
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => (info(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => (warn(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => (error(format_args!($($arg)*)));
+}
+
+pub fn info(msg: std::fmt::Arguments) {
+    println!(
+        "{} {}: {}",
+        "[INFO]".green().bold(),
+        format!("[{}]", Local::now().format("%Y-%m-%d %H:%M:%S")).yellow(),
+        msg
+    );
+}
+
+pub fn warn(msg: std::fmt::Arguments) {
+    println!(
+        "{} {}: {}",
+        "[WARN]".purple().bold(),
+        format!("[{}]", Local::now().format("%Y-%m-%d %H:%M:%S")).yellow(),
+        msg
+    );
+}
+
+pub fn error(msg: std::fmt::Arguments) {
+    println!(
+        "{} {}: {}",
+        "[ERROR]".red().bold(),
+        format!("[{}]", Local::now().format("%Y-%m-%d %H:%M:%S")).yellow(),
+        msg
+    );
+}
